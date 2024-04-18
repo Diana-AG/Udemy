7db8e43c869702d117391efacd5b260a7ef6aada
Diana Georgieva <deedee.ag@gmail.com>
2024-04-18 16:13:42 +0300

    Implement Accordion and AccordionItem Components

--- 052-accordion-component/src/App.js
+++ 052-accordion-component/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const faqs = [
   {
     title: "Where are these chairs assembled?",
@@ -15,12 +16,41 @@ const faqs = [
 
 export default function App() {
   return (
-    <div>
-      <Accordion />
+    <div className="Accordion">
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-function Accordion() {
-  return <div>TODO</div>;
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem
+          number={i}
+          title={el.title}
+          text={el.text}
+          key={el.title}
+        />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ number, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{number < 10 ? `0${number + 1}` : number + 1}</p>
+      <p className="title">{title}</p>
+      <p classname="icon">{isOpen ? "-" : "+"}</p>
+
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
 }
